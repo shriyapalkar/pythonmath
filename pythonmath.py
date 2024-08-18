7c3ed1009adc166965ca81f76f4e900867dd9d17
shriyapalkar <132357599+shriyapalkar@users.noreply.github.com>
2024-08-18 18:39:23 +0530

    Add files via upload

---
+++ pythonmath.py
@@ -0,0 +1,28 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+#The abs() function returns the absolute (positive) value of the specified number:
+#Example
+x = abs(-7.25)
+print(x)
+
+#The pow(x, y) function returns the value of x to the power of y (xy).
+#Example
+#Return the value of 4 to the power of 3 (same as 4 * 4 * 4):
+
+x = pow(4,3)
+print(x)
+
+import math
+#x = math.sqrt(64)
+#print(x)
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)
+print(y)
+
